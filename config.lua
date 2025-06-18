b1d788bd313b412373e224d16fe0da701f72cdfc
Marck Carrion Guzman <mcarrion@kcramsolutions.com>
2025-06-18 11:52:39 +0200

    chore: add assets and examples

---
+++ config.lua
@@ -0,0 +1,22 @@
+config = {
+    log_file_path = "./var/log/styx",
+    database_path = "./lib/routes.db",
+    some_option = "valor opcional",
+    log_level = "info",
+    log_to_console = true,
+    log_to_file = true,
+    log_format_json = true,
+    cache_TTL = 36000,
+    auth_url = "http://localhost:3000/v1/auth/check",
+    token_header= "api-token"
+}
+http_proxy = {
+    port = 3080,
+    timeout = 30000,
+    address = "0.0.0.0"
+}
+
+admin = {
+    port = 3082,
+    address = "0.0.0.0"
+}
